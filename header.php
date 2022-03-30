558afc39fa8ef59966dda25536dd89a06aeef74f
JeroenZwaal <93530512+JeroenZwaal@users.noreply.github.com>
2022-03-30 11:24:46 +0200

    added links

--- header.php
+++ header.php
@@ -1,3 +1,10 @@
+<div class"wrapper">
+<div class"navbar">
+<nav>
 <a href="index.php">Home</a>
 <a href="">Karretjes</a>
 <a href="">Contact</a>
+<img src="img/placeholder.png" alt="Logo">
+</nav>
+</div>
+</div>
