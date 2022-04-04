2d44dff3db23d92d8756e598424a2cd40389d85e
KelvinMies <Kelvinmies@hotmail.com>
2022-04-04 14:43:20 +0200

    constructiepagina gemaakt

--- constructie.php
+++ constructie.php
@@ -21,3 +21,30 @@
 
   <meta name="theme-color" content="#fafafa">
 </head>
+<?php require_once("header.php") ?>
+<body>
+  <div class="imageConstructie">
+    <img src="img/achtbaanconstructie.jpg">
+  </div>
+  <div class="wrapper">
+    <div class="tekstConstructie">
+      <h1>De constructie</h1>
+      <p>Het eerste USP die wij voor deze achtbaan hebben ontwikkeld is een nieuwe metaal messing die nog nooit eerder in deze industrie gebruikt is. Door een menging te maken van 69% staal, 20% nikkel, en 11% koper hebben een nieuwe messing ontwikkeld die wij nu MagnaAlloy noemen. Met deze nieuwe MagnaAlloy maken wij een aantal onderdelen die de meeste stress ondergaan in de achtbaan, waardoor deze nog meer G-krachten kunnen weerstaan, en langer meegaan dan andere stalen. We hebben enorm geprobeerd de kosten zo laag mogelijk te houden, dat de onderdelen gemiddeld slechts 8% duurder uitvallen dan andere soortgelijke onderdelen, zonder kwaliteitsverlies. Onze tests bewijzen ook dat de onderdelen tot 25% langer meegaan, dus dit zou uiteindelijk ook in kosten moeten besparen. Ook zorgt dit ervoor dat een aantal designs voor achtbanen die eerder niet mogelijk waren nu wel mogelijk zijn.</p>
+    </div>
+  </div>
+
+
+<!-- Add your site or application content here -->
+  <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+<?php require_once("footer.php") ?>
+</html>
